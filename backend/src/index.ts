4f2982472f467b0bab241dcadbf4241c47a1c296
jbyomuhangi <jbyomuhangi@gmail.com>
2023-01-06 02:17:25 +0800

    Set up connection to mysql database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,22 @@
 import "reflect-metadata";
+import { DataSource } from "typeorm";
 
-console.log("hello world!");
+const main = async () => {
+  const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "joel",
+    password: "admin",
+    database: "ig_clone_db",
+    entities: [],
+    //   synchronize: true,
+    logging: true,
+  });
+
+  const dataSource = await AppDataSource.initialize();
+};
+
+main().catch((err) => {
+  console.error(err);
+});
